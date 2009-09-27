be02a23229f7ca493950f97780f4bdb183325dda
amir <amir@viel-zu.org>
2009-09-27 22:45:11 +0000

    corrected miscommit
    git-svn-id: https://whatever.metalab.at/user/amir/EpilogCups@1000 b84d55d7-753a-4893-9975-3c41facd0aad

--- KallaBalla/KallaBalla/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cp -Rd /home/amir/dev/wooden/bin/* /home/amir/dev/KallaBalla/deploy/
-cp -Rd /home/amir/dev/KallaBalla/bin/* /home/amir/dev/KallaBalla/deploy/
-PWD=`pwd`
-cd /home/amir/dev/KallaBalla/deploy/
-jar cf /tmp/kallaBalla.jar -C /home/amir/dev/KallaBalla/deploy com DisplayModeModel.class DisplayModeTest.class DisplayModeTest.java gnu javax JfcExample.class JfcExample.java junit META-INF org stylesheet.css
-cp /tmp/kallaBalla.jar /var/www/kallaBalla.jar
-cd $PWD
