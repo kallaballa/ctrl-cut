b4b7ab99a6ca72f2efa1640fe33caae811235ee3
amir <amir@viel-zu.org>
2009-12-02 14:45:47 +0000

    removed conditional filter configuration code
    git-svn-id: https://whatever.metalab.at/user/amir/EpilogCups@1047 b84d55d7-753a-4893-9975-3c41facd0aad

--- src/Filter.h
+++ src/Filter.h
@@ -31,109 +31,7 @@ using std::string;
 using std::cerr;
 using std::endl;
 
-class ConditionBase {
-public:
-	template<class T>
-	bool eval(const T actualValue);
-};
-
-template<class T>
-class Condition: public ConditionBase {
-public:
-	T expectedValue;
-	Condition(const T expectedValue) {
-		this->expectedValue = expectedValue;
-	}
-	bool eval(const T actualValue) {
-		return this->expectedValue == actualValue;
-	}
-};
-
-template<class T>
-class BasicCondition: public Condition<T> {
-
-public:
-	enum Comparator {
-		eq, ne, lt, le, gt, ge, cmp, ncmp
-	};
-	Comparator comp;
-
-	BasicCondition(const T expectedValue, const Comparator comp) :
-		Condition<T> (expectedValue) {
-		this->comp = comp;
-	}
-
-	bool eval(const T actualValue) {
-		switch (comp) {
-		case eq:
-			return this->expectedValue == actualValue;
-		case ne:
-			return this->expectedValue != actualValue;
-		case lt:
-			return this->expectedValue > actualValue;
-		case le:
-			return this->expectedValue >= actualValue;
-		case gt:
-			return this->expectedValue < actualValue;
-		case ge:
-			return this->expectedValue <= actualValue;
-		case cmp:
-			return ((string) this->expectedValue).compare((string) actualValue)
-					== 0;
-		case ncmp:
-			return ((string) this->expectedValue).compare((string) actualValue)
-					!= 0;
-		}
-		//should never happen since Comparator is an enum
-		return false;
-	}
-};
-
-
-
-template<class T>
-class ChainCondition: public Condition<T> {
-public:
-	enum Junctor {
-		_and, _or
-	};
-
-	struct CCPair {
-		Condition<T>* condition;
-		Junctor* junctor;
-	};
-
-	list<CCPair*> conditionList;
-
-	ChainCondition() : Condition<T>(NULL) {
-	}
-
-	ChainCondition* connect(Condition<T>* condition, Junctor* junctor) {
-		CCPair ccp = { condition, junctor };
-		conditionList.push_back(&ccp);
-		return this;
-	}
-
-	bool eval(T actualValue) {
-		if(conditionList.size() == 0)
-			return false;
-
-		bool eval = (*conditionList.begin())->condition->eval(actualValue);
-		conditionList.erase(conditionList.begin());
-/*
-		for(list<CCPair*>::iterator it = conditionList.begin(); it != conditionList.end(); it++) {
-			CCPair* ccp = *it;
-			if(ccp->junctor == _and)
-				eval = eval && ccp->condition->eval(actualValue);
-			else
-				eval = eval || ccp->condition->eval(actualValue);
-		}*/
-		return eval;
-	}
-};
-
 typedef string FilterID;
-typedef map<string, ConditionBase*> FilterConditionMap;
 typedef set<FilterID > FilterList;
 
 class Filter {
@@ -141,24 +39,22 @@ public:
 	FilterList provides;
 	FilterList preFilters;
 	FilterList postFilters;
-	FilterConditionMap conditions;
 };
 
-//TODO find out how to do interface class declarations without getting "variable not used" warning
 class RasterFilter: public Filter {
 public:
 
 	RasterFilter(){}
-	virtual ~RasterFilter(){}
-	void filter(RasterPass* p){ p = p; };
+	virtual ~RasterFilter(){};
+	virtual void filter(RasterPass* p) = 0;
 };
 
 class VectorFilter: public Filter {
 public:
 
 	VectorFilter(){}
-	virtual ~VectorFilter(){}
-	void filter(VectorPass* p){ p = p; };
+	virtual ~VectorFilter(){};
+	virtual void filter(VectorPass* p) = 0;
 };
 
 #endif /* FILTER_H_ */
