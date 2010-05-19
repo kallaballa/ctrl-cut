d9d4cc8b2351a8f9958bbd4c7a927fe78c0c757a
amir <amir@viel-zu.org>
2010-05-19 12:18:03 +0000

    experiments to understand the internal tiff encoding of pcl
    git-svn-id: https://whatever.metalab.at/user/amir/EpilogCups@1348 b84d55d7-753a-4893-9975-3c41facd0aad

---
+++ src/PCLTiffEncoder.h
@@ -0,0 +1,17 @@
+/*
+ * PCLTiffEncoder.h
+ *
+ *  Created on: Dec 12, 2009
+ *      Author: elchaschab
+ */
+
+#ifndef PCLTIFFENCODER_H_
+#define PCLTIFFENCODER_H_
+
+class PCLTiffEncoder {
+public:
+	PCLTiffEncoder();
+	virtual ~PCLTiffEncoder();
+};
+
+#endif /* PCLTIFFENCODER_H_ */
