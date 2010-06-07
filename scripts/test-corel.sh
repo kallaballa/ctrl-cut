81a59da83ab7fee1d42339d430602bc5ae96507f
Marius Kintel <marius@kintel.net>
2010-06-07 02:26:21 +0000

    Extended to measure num polylines and total cut length
    git-svn-id: https://whatever.metalab.at/user/amir/EpilogCups@1408 b84d55d7-753a-4893-9975-3c41facd0aad

--- scripts/test-corel.sh
+++ scripts/test-corel.sh
@@ -2,25 +2,53 @@
 
 #set -x 
 
+function pad {
+  echo -n $1
+  s=$(printf "%$(($2-${#1}))s"); echo -n "${s// /.}"
+}
+
 function runtest {
   testcase=`basename $1 .ps`
-  echo -n "*$testcase...."
+  corelfile=$srcdir/$testcase.prn
+  pad "*$testcase" 22
   outfile=test-data/corel/$testcase.raw
   scripts/run-filter.sh $1 > $outfile 2> $testcase.log
   diff -a $srcdir/$testcase.prn $outfile >> $testcase.log
   if [ $? == 0 ]; then
-    echo OK
+    echo -n OK
   else
-    echo -n "binaries differ, comparing vectors..."
-    scripts/prn-to-pbm.sh $srcdir/$testcase.prn
+    pad "no" 5
+    scripts/prn-to-pbm.sh $corelfile
     scripts/prn-to-pbm.sh $outfile
     errorstr=`scripts/compare-bitmaps.sh $srcdir/$testcase.prn.pbm $outfile.pbm`
     if [ $? == 0 ]; then
-      echo OK
+      pixelstr="OK"
     else
-      echo "Failed ($errorstr)"
+      pixelstr=`echo $errorstr | awk '{ print $3 }'`
     fi
+    pad "$pixelstr" 7
+
+    infoA=`python/rtlinfo.py $corelfile`
+    plA=`echo $infoA | awk '{print $2}'`
+    lenA=`echo $infoA | awk '{print $4}'`
+    infoB=`python/rtlinfo.py $outfile`
+    plB=`echo $infoB | awk '{print $2}'`
+    lenB=`echo $infoB | awk '{print $4}'`
+
+    if [ $plA != $plB ]; then
+        plstr="$plA->$plB"
+    else
+        plstr="OK"
+    fi
+    pad $plstr 14
+    if [ $lenA != $lenB ]; then
+        lenstr="$lenA->$lenB"
+    else
+        lenstr="OK"
+    fi
+    echo -n $lenstr
   fi
+  echo
 }
 
 if [ $# -gt 1 ]; then
@@ -32,7 +60,12 @@ if [ $# == 1 ]; then
   srcdir=`dirname $1`
   runtest $1
 else
-  echo "Running tests"
+  pad "Test case" 22
+  pad "bin" 5
+  pad "img" 7
+  pad "polylines" 14
+  echo -n "length"
+  echo
   srcdir=test-data/corel
   for f in $srcdir/*.ps; do
     runtest $f
