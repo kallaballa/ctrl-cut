97eb3a390443bc189ec29ce57d3a7cf4c1f75098
amir <amir@viel-zu.org>
2010-12-15 14:59:03 +0000

    made it a little more platform independent
    git-svn-id: https://whatever.metalab.at/user/amir/EpilogCups@1521 b84d55d7-753a-4893-9975-3c41facd0aad

--- scripts/run-filter.sh
+++ scripts/run-filter.sh
@@ -1,25 +1,29 @@
 #!/bin/sh
 
+function error() {
+    echo "Error: $1"
+    exit $2
+}
+
 filter=./EpilogFilter
-if [ $# != 1 ]; then
-  echo "Usage: $0 ps-file"
-  exit 1
-fi
+[ $# != 1 ] && error "Usage: $0 ps-file" 1
 file=$1
+
+type cups-config || error "cups-config required" 2
 
 export CHARSET=utf-8
 export CONTENT_TYPE=application/pdf
-export CUPS_CACHEDIR=/private/var/spool/cups/cache
-export CUPS_DATADIR=/usr/share/cups
+#export CUPS_CACHEDIR=/private/var/spool/cups/cache
+export CUPS_DATADIR=`cups-config --datadir`
 export CUPS_FILETYPE=document
-export CUPS_SERVERROOT=/private/etc/cups
+export CUPS_SERVERROOT=`cups-config --serverroot`
 export DEVICE_URI=file:/tmp/passthrough.ps
 export FINAL_CONTENT_TYPE=printer/passthrough
 export LANG=en_US.UTF-8
-export PATH=/usr/libexec/cups/filter:/usr/bin:/usr/sbin:/bin:/usr/bin
-export PPD=/private/etc/cups/ppd/passthrough.ppd
+#export PATH=/usr/libexec/cups/filter:/usr/bin:/usr/sbin:/bin:/usr/bin
+export PPD="$CUPS_DATADIR/ppd/passthrough.ppd"
 export PRINTER=passthrough
-export SOFTWARE=CUPS/1.4.3
+#export SOFTWARE=CUPS/1.4.3
 export USER=root
 
 $filter 32 kintel `basename $file ps`cdr 1 options $file
