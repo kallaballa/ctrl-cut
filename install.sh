ea3ee4ece456ae29020596ee82271dd1a6bd52d8
amir <amir@viel-zu.org>
2011-01-08 01:32:41 +0100

    minimal install script to do chroot tests

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# cd to epilogcups basedir
+cd "`dirname $(readlink -f $0)`"
+
+[ ! $# -eq 2 ] && echo "install.sh PRINTERNAME PPDNAME"
+
+PRINTER=$1
+PPDNAME=$2
+FILTER_PATH="`cups-config --serverbin`/filter"
+
+
+# generate ppd files
+etc/ppd/generate_ppds.sh
+
+# copy epilog filter into the cups filter path
+cp EpilogFilter $FILTER_PATH/epilogcups
+
+# install the printer
+lpadmin -p "$PRINTER" -P "etc/ppd/$PPDNAME"
+
