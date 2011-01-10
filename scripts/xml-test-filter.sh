6354459df7b36ee395493ecfb6fbc56c13ec2ab8
amir <amir@viel-zu.org>
2011-01-11 00:16:13 +0100

    fixed basename path issue

--- scripts/xml-test-filter.sh
+++ scripts/xml-test-filter.sh
@@ -10,11 +10,13 @@ function verbose  { [ $VERBOSE ] && echo $@; $@; }
 function error { echo "error: $1" >&2; exit 1; }
 function check { 
     echo -en "\tcheck: $1 $2 "
+    cd "$1"
     cmd="$XQ -i epilog.xml $2"
     [ $VERBOSE ] && echo -n "($cmd)";
     echo -n " ... "
     result="`$cmd`"
     [ "$result" == "0" ] && green "OK" || red $result
+    cd ..
 }
 
 ### main
@@ -32,7 +34,7 @@ done
 for filter in "$@"; do
     echo "testing $filter..."
     for xql in `ls $filter/*.xql`; do
-        check $filter $xql;
+        check `basename $filter` `basename $xql`;
     done
 done
 
