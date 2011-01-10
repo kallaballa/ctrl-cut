1cf217e715ddbf06452e51636e125042a9c3a535
amir <amir@viel-zu.org>
2011-01-10 13:37:57 +0100

    dont use absolute paths for ppd filters

--- scripts/generate_ppds.sh
+++ scripts/generate_ppds.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-TEST_FILTER_PATH="$EC_TEST_CODE"
-FILTER_PATH="`cups-config --serverbin`/filter"
-
-sed "s|\$FILTER|$TEST_FILTER_PATH/passthroughfilter|g" $EC_PPD_DIR/stubs/passthrough.ppd > $EC_PPD_DIR/passthrough.ppd
-sed "s|\$FILTER|$FILTER_PATH/epilogcups|g" $EC_PPD_DIR/stubs/EpilogLegend36EXT.ppd > $EC_PPD_DIR/EpilogLegend36EXT.ppd
-sed "s|\$FILTER|$FILTER_PATH/epilogcups|g" $EC_PPD_DIR/stubs/EpilogPostscript.ppd > $EC_PPD_DIR/EpilogPostscript.ppd
+sed "s|\$FILTER|passthroughfilter|g" $EC_PPD_DIR/stubs/passthrough.ppd > $EC_PPD_DIR/passthrough.ppd
+sed "s|\$FILTER|epilogcups|g" $EC_PPD_DIR/stubs/EpilogLegend36EXT.ppd > $EC_PPD_DIR/EpilogLegend36EXT.ppd
+sed "s|\$FILTER|epilogcups|g" $EC_PPD_DIR/stubs/EpilogPostscript.ppd > $EC_PPD_DIR/EpilogPostscript.ppd
 
