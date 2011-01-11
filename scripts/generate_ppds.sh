e61772debf6409d6ac25e15690ebf2276fe85783
amir <amir@viel-zu.org>
2011-01-11 23:31:37 +0100

    we dont need to generate ppd files

--- scripts/generate_ppds.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sed "s|\$FILTER|passthroughfilter|g" $EC_PPD_DIR/stubs/passthrough.ppd > $EC_PPD_DIR/passthrough.ppd
-sed "s|\$FILTER|epilogcups|g" $EC_PPD_DIR/stubs/EpilogLegend36EXT.ppd > $EC_PPD_DIR/EpilogLegend36EXT.ppd
-sed "s|\$FILTER|epilogcups|g" $EC_PPD_DIR/stubs/EpilogPostscript.ppd > $EC_PPD_DIR/EpilogPostscript.ppd
-
