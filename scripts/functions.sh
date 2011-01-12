e1c05e56aabf79827b8a00caea7cc69305b5d8e9
amir <amir@viel-zu.org>
2011-01-12 05:05:43 +0100

    introduce verbose function and minor fixes

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -4,12 +4,12 @@ function green { echo -e "\033[32;1m $1 \033[0m" 1>&2; tput sgr0; }
 function red { echo -e "\033[31;1m $1 \033[0m" 1>&2; tput sgr0; }
 function warn { red "$1"; }
 function ok { green "ok"; }
-
+function verbose { [ ! -z $VERBOSE ] && echo "$@ " 1>&2; }
 function error { 
     msg=$1; 
     errcode=$2; 
-    [ -z $errcode ] && errcode=1; 
-    [ -z $msg ] && msg="failed"; 
+    [ -z "$errcode" ] && errcode=1; 
+    [ -z "$msg" ] && msg="failed"; 
     red "$msg"; exit $errcode; 
 }
 
@@ -18,8 +18,9 @@ function try {
     echo -n "$1 " 1>&2; 
     shift;
 
-    [ $VERBOSE ] && echo -n "$@ " 1>&2; 
-    $@ 1>&2; errcode=$?; 
+    verbose $@; $@ 1>&2;
+    errcode=$?; 
+    
     [ $errcode ]  && ok || error; 
     return $errcode; 
 }
