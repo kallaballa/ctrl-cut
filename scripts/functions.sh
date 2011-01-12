6f8445d1b42a1f6cc269350a5ba6b6f5516d4932
amir <amir@viel-zu.org>
2011-01-12 01:58:48 +0100

    redirect try output to stderr

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-function green { echo -e "\033[32;1m $1 \033[0m"; tput sgr0; }
-function red { echo -e "\033[31;1m $1 \033[0m"; tput sgr0; }
+function green { echo -e "\033[32;1m $1 \033[0m" &>&2; tput sgr0; }
+function red { echo -e "\033[31;1m $1 \033[0m" &>&2; tput sgr0; }
 function warn { red "$1"; }
 function ok { green "ok"; }
 
@@ -10,15 +10,15 @@ function error {
     errcode=$2; 
     [ -z $errcode ] && errcode=1; 
     [ -z $msg ] && msg="failed"; 
-    red "$msg" >&2; exit $errcode; 
+    red "$msg"; exit $errcode; 
 }
 
 function try { 
     errcode=0; 
-    echo -n "$1 "; 
+    echo -n "$1 " &>&2; 
     shift;
 
-    [ $VERBOSE ] && echo -n "$@ "; 
+    [ $VERBOSE ] && echo -n "$@ " &>&2; 
     $@ 1> /dev/null; errcode=$?; 
     [ $errcode ]  && ok || error; 
     return $errcode; 
