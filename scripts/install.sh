f0ea3037d7a2f86243665012d7be2069959179b4
amir <amir@viel-zu.org>
2011-01-19 14:53:47 +0100

    chrooot script fixes

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,8 +17,8 @@ try "find cups-config..." "type -p cups-config"
 FILTER_PATH="`cups-config --serverbin`/filter"
 BACKEND_PATH="`cups-config --serverbin`/backend"
 
-try "Installing filter binary..." "cp $CC_BINARY $FILTER_PATH/$CC_BINARY"
+try "Installing filter binary..." "cp $CC_BINARY $FILTER_PATH/"
 try "Installing dump backend..." "cp $CC_TEST_CODE/dump $BACKEND_PATH"
 try "Installing passthrough filter" "cp $CC_TEST_CODE/passthroughfilter $FILTER_PATH"
-try "Installing the passthrough filter" "/usr/sbin/lpadmin -p \"$PRINTER\" -P \"$CC_PPD_DIR/$PPDNAME\" -v \"$DEVICEURL\""
+echo "/usr/sbin/lpadmin -p \"$PRINTER\" -P \"$CC_PPD_DIR/$PPDNAME\" -v \"$DEVICEURL\""
 
