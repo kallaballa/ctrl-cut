1268e803d0722379d0cfef85be3a46689d1f2db2
Marius Kintel <marius@kintel.net>
2011-01-26 02:56:09 +0100

    Updated package to a more minimal setup, tuning of the build script

--- scripts/publish-macosx.sh
+++ scripts/publish-macosx.sh
@@ -9,22 +9,36 @@ export MACOSX_DEPLOY_DIR=$PWD/../libraries/install
 echo "Building ctrl-cut-$VERSION..."
 qmake VERSION=$VERSION CONFIG+=deploy CONFIG-=debug ctrl-cut.pro
 make -s clean
-make -j2
+make -j4
 if [[ $? != 0 ]]; then
   exit 1
 fi
 
-INSTALL_LIB_DIR=/Library/Printers/CtrlCut/lib
-
-install_name_tool -change /lib/libgs.9.00.dylib ${INSTALL_LIB_DIR}/libgs.9.00.dylib ctrl-cut
-install_name_tool -id libgs.9.00.dylib ${INSTALL_LIB_DIR}libgs.9.00.dylib
-
+INSTALL_PRINTERS_DIR=/Library/Printers
+INSTALL_CTRLCUT_DIR=${INSTALL_PRINTERS_DIR}/Ctrl-Cut
+INSTALL_FILTER_DIR=/usr/libexec/cups/filter
 
 echo "Sanity check of the executable..."
-DYLD_LIBRARY_PATH=${INSTALL_LIB_DIR} `dirname $0`/macosx-sanity-check.py ctrl-cut
+DYLD_LIBRARY_PATH=${MACOSX_DEPLOY_DIR}/lib `dirname $0`/macosx-sanity-check.py ctrl-cut
 if [[ $? != 0 ]]; then
   exit 1
 fi
 
-echo "FIXME: Create installer package"
+echo "Building package..."
+# Point the executable to the installed location of the libraries
+install_name_tool -change libgs.9.00.dylib ${INSTALL_CTRLCUT_DIR}/lib/libgs.9.00.dylib ctrl-cut
+
+# Copy files to folder structure
+mkdir -p root${INSTALL_CTRLCUT_DIR}/Icons
+cp images/EpilogLegend36EXT.icns root${INSTALL_CTRLCUT_DIR}/Icons
+mkdir -p root${INSTALL_CTRLCUT_DIR}/lib
+cp ${MACOSX_DEPLOY_DIR}/lib/libgs.9.00.dylib root${INSTALL_CTRLCUT_DIR}/lib
+mkdir -p root${INSTALL_PRINTERS_DIR}/PPDs/Contents/Resources
+cp etc/ppd/EpilogLegend36EXT.ppd root${INSTALL_PRINTERS_DIR}/PPDs/Contents/Resources
+mkdir -p root${INSTALL_FILTER_DIR}
+cp ctrl-cut root${INSTALL_FILTER_DIR}
+
+# Build package
 /Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker --doc packaging/Ctrl-Cut.pmdoc --out Ctrl-Cut.pkg
+
+rm -r root
