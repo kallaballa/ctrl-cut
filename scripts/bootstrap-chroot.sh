ac468851e2b80a03f72c3cd8a8e97a78f90c757f
amir <amir@viel-zu.org>
2011-02-05 04:17:57 +0100

    added libgs dep to chroot bootstrap

--- scripts/bootstrap-chroot.sh
+++ scripts/bootstrap-chroot.sh
@@ -11,7 +11,7 @@ KERNEL=`uname -r`
 try "Debootstrap: $CC_CHROOT_FLAVOUR into $CC_TEST_CHROOT"      "debootstrap --verbose $CC_CHROOT_FLAVOUR $CC_TEST_CHROOT $CC_CHROOT_MIRROR"
 chrtrun "Create kernel mod directory"               		"mkdir /lib/modules/$KERNEL/"
 chrtrun "Generate modules.dep"  				"depmod -a"
-chrtrun "Install dependencies and build tool chain"             "apt-get -y --allow-unauthenticated install build-essential cups libcups2-dev libboost-dev qt4-qmake git-core libqtgui4"
+chrtrun "Install dependencies and build tool chain"             "apt-get -y --allow-unauthenticated install build-essential cups libcups2-dev libgs-dev libboost-dev qt4-qmake git-core libqtgui4"
 chrtrun "install packages required for testing"                 "apt-get -y --allow-unauthenticated install pkg-config python-minimal xqilla librsvg2-dev libcairo2 unzip"
 chrtrun "Fixing qt lib symlinks"                                "ln -s /usr/lib/libQtCore.so.4 /usr/lib/libQtCore.so; ln -s /usr/lib/libQtGui.so.4 /usr/lib/libQtGui.so"
 
