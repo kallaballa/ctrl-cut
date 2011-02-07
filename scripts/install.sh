d249c207d9488cc22d4c99dc00e167a0f03bc429
amir <amir@viel-zu.org>
2011-02-08 00:54:38 +0100

    fix file permissions

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,5 +18,6 @@ FILTER_PATH="`cups-config --serverbin`/filter"
 BACKEND_PATH="`cups-config --serverbin`/backend"
 
 try "Installing filter binary..." "cp $CC_BINARY $FILTER_PATH/"
-try "Installing dump backend..." "cp $CC_TEST_CODE/dump $BACKEND_PATH ; chmod u+x $BACKEND_PATH/dump"
+try "Installing dump backend..." "cp $CC_TEST_CODE/dump $BACKEND_PATH"
+try "Fixing file permissions..." "chmod u+x $BACKEND_PATH/dump"
 try "Installing printer Name: $PRINTER PPD: $PPDFILE Device:$DEVICEURL..." "lpadmin -p $PRINTER -P $PPDFILE -v $DEVICEURL"
