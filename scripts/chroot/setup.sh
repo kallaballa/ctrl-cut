140ec15d497bd1b3155afecb94c25acecd47469d
amir <amir@viel-zu.org>
2011-02-08 17:09:44 +0100

    verbose env

--- scripts/chroot/setup.sh
+++ scripts/chroot/setup.sh
@@ -50,7 +50,7 @@ try "Install" "python setup.py install"
 try "Clean up" "rm -r /tmp/pysvg-*"
 
 cd $BUILD_DIR
-CC_BASE=$BUILD_DIR . ./cc
+CC_BASE=$BUILD_DIR . ./cc -v
 try "Generate Makefile" "qmake -recursive"
 try "Build" "make"
 try "Installing printer" "./cc install -p $CC_PRINTERNAME $CC_PPD_DIR/$CC_PRINTERPPD dump://"
