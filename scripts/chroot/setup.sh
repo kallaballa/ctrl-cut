e8f61b24a050c32c0d749d7f7a7c1cff6f6dc18e
amir <amir@viel-zu.org>
2011-02-08 06:05:50 +0100

    fixed indent

--- scripts/chroot/setup.sh
+++ scripts/chroot/setup.sh
@@ -16,20 +16,20 @@ BUILD_DIR=$1
 
 [ $CLEAR ] && try "Clear build directory" "rm -r $BUILD_DIR"
 if [ -d $BUILD_DIR ]; then 
-    try "Pull ctrl-cut"         "git --git-dir=$BUILD_DIR pull"
+    try "Pull" "git --git-dir=$BUILD_DIR pull"
 else
     try "Clone" "git --git-dir=$BUILD_DIR clone $CC_GIT_URL"
 fi
 
 cd /tmp
-try "Download pysvg"        "wget $CC_PYSVG_URL"
-try "Unpack"                "unzip pysvg-*.zip"
+try "Download pysvg" "wget $CC_PYSVG_URL"
+try "Unpack" "unzip pysvg-*.zip"
 cd pysvg-*
-try "Install"               "python setup.py install"
-try "Clean up"              "rm -r pysvg-*"
+try "Install" "python setup.py install"
+try "Clean up" "rm -r pysvg-*"
 
 cd $BUILD_DIR
-try "Generate Makefile"         "qmake -recursive"
-try "Build"                     "make"
-try "Installing printer"        "./cc install $CC_PRINTERNAME $CC_PPD_DIR/$CC_PRINTERPPD dump://"
+try "Generate Makefile" "qmake -recursive"
+try "Build" "make"
+try "Installing printer" "./cc install $CC_PRINTERNAME $CC_PPD_DIR/$CC_PRINTERPPD dump://"
 
