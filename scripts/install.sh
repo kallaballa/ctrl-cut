5266c4677457bf2cfddd63f46acd70acde7d6120
amir <amir@viel-zu.org>
2011-02-12 12:04:04 +0100

    removed debug output

--- scripts/install.sh
+++ scripts/install.sh
@@ -22,9 +22,7 @@ FILTER_PATH="$CUPS_SERVER_BIN/filter"
 BACKEND_PATH="$CUPS_SERVER_BIN/backend"
 
 try "Install filter binary $FILTER_PATH" "cp $CC_BINARY $FILTER_PATH/"
-ls $FILTER_PATH/
 try "Install lpd backend $BACKEND_PATH" "cp $CC_BASE/src/lpd-epilog/lpd-epilog $BACKEND_PATH/"
-ls $BACKEND_PATH/
 #try "Installing dump backend" "cp $CC_TEST_CODE/dump $BACKEND_PATH"
 #try "Fixing file permissions" "chmod u+x $BACKEND_PATH/dump"
 [ $PRINTER_INSTALL ] && try "Installing printer Name: $PRINTER PPD: $PPDFILE Device:$DEVICEURL" "lpadmin -E -p $PRINTER -P $PPDFILE -v $DEVICEURL"
