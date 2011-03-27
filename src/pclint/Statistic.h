9d02800d502d251cd354798ecc2f8f546b6f41aa
amir <amir@viel-zu.org>
2011-03-27 04:58:16 +0200

    track bounding box in statistics

--- src/pclint/Statistic.h
+++ src/pclint/Statistic.h
@@ -20,6 +20,7 @@
 #ifndef STATISTIC_H_
 #define STATISTIC_H_
 
+#include "2D.h"
 
 enum STAT_SLOT { SLOT_RASTER, SLOT_VECTOR, SLOT_GLOBAL };
 enum STAT_UNIT { UNIT_MM, UNIT_IN, UNIT_PPT };
@@ -31,6 +32,7 @@ public:
   uint32_t penDownCnt;
   uint32_t penUpCnt;
   uint32_t segmentCnt;
+  BoundingBox bbox;
 
   Slot(): workLen(0), moveLen(0), penDownCnt(0), penUpCnt(0) {
   }
@@ -40,7 +42,7 @@ public:
 
 class Statistic {
 private:
-  const Slot *slots;
+  Slot *slots;
   const double in_factor;
   const double mm_factor;
   static Statistic* instance;
@@ -48,10 +50,9 @@ private:
 public:
   static Statistic* singleton();
 
-  Statistic(uint16_t resolution) : slots(new Slot[3]),  in_factor(10 / resolution), mm_factor(25.5/ resolution){
+  Statistic(uint16_t resolution) : slots(new Slot[2]),  in_factor(10 / resolution), mm_factor(25.5/ resolution){
     slots[SLOT_RASTER] = *(new Slot());
     slots[SLOT_VECTOR] = *(new Slot());
-    slots[SLOT_GLOBAL] = *(new Slot());
   };
 
   virtual ~Statistic() {};
@@ -63,6 +64,8 @@ public:
   void announceWork(const Point& from, const Point& to, const STAT_SLOT slot) {
     slots[slot].workLen += distance(from, to);
     slots[slot].segmentCnt++;
+    slots[slot].bbox.update(from);
+    slots[slot].bbox.update(to);
   }
 
   void announceMove(const Point& from, const Point& to, const STAT_SLOT slot) {
@@ -89,27 +92,56 @@ public:
   }
 
   double getWorkLength(const STAT_SLOT slot, const STAT_UNIT unit=UNIT_MM) const {
-    return convert(slots[slot].workLen, unit);
+    if(slot != SLOT_GLOBAL)
+      return convert(slots[slot].workLen, unit);
+    else
+      return convert(slots[0].workLen + slots[1].workLen, unit);
   }
 
   double getMoveLength(const STAT_SLOT slot, const STAT_UNIT unit=UNIT_MM) const {
-    return convert(slots[slot].moveLen, unit);
+    if(slot != SLOT_GLOBAL)
+      return convert(slots[slot].moveLen, unit);
+    else
+      return convert(slots[0].moveLen + slots[1].moveLen, unit);
   }
 
   double getTotalLength(const STAT_SLOT slot, const STAT_UNIT unit=UNIT_MM) const {
-    return convert(slots[slot].workLen + slots[slot].moveLen, unit);
+    if(slot != SLOT_GLOBAL)
+      return convert(slots[slot].workLen + slots[slot].moveLen, unit);
+    else
+      return convert(slots[0].workLen + slots[0].moveLen + slots[1].workLen + slots[1].moveLen, unit);
   }
 
   uint32_t getPenUpCount(const STAT_SLOT slot) const {
-    return slots[slot].penUpCnt;
+    if(slot != SLOT_GLOBAL)
+      return slots[slot].penUpCnt;
+    else
+      return slots[0].penUpCnt + slots[1].penUpCnt;
   }
 
   uint32_t getPenDownCount(const STAT_SLOT slot) const {
-    return slots[slot].penUpCnt;
+    if(slot != SLOT_GLOBAL)
+      return slots[slot].penDownCnt;
+    else
+      return slots[0].penDownCnt + slots[1].penDownCnt;
   }
 
   uint32_t getSegmentCount(const STAT_SLOT slot) const {
-    return slots[slot].penUpCnt;
+    if(slot != SLOT_GLOBAL)
+      return slots[slot].segmentCnt;
+    else
+      return slots[0].segmentCnt + slots[1].segmentCnt;
+  }
+
+  BoundingBox& getBoundingBox(const STAT_SLOT slot) const {
+    if(slot != SLOT_GLOBAL)
+      return slots[slot].bbox;
+    else {
+      BoundingBox& globalBBox = *(new BoundingBox());
+      globalBBox += slots[0].bbox;
+      globalBBox += slots[1].bbox;
+      return globalBBox;
+    }
   }
 
   void printSlot(ostream& os, const STAT_SLOT slot, const STAT_UNIT unit=UNIT_MM) const {
@@ -132,6 +164,7 @@ public:
     os << slotName << "\t| penUp count: " << getPenUpCount(slot) << endl;
     os << slotName << "\t| penDown count: " << getPenDownCount(slot) << endl;
     os << slotName << "\t| segment count: " << getSegmentCount(slot) << endl;
+    os << slotName << "\t| bounding box: " << getBoundingBox(slot) << endl;
   }
 };
 
