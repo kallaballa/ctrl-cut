2cb5fc5cc4da971d741f4eb78c5c60315c024b70
amir <amir@viel-zu.org>
2011-04-15 09:43:55 +0200

    fix canvas of rendered vector tests if bbox doesn'T match

--- scripts/rtlcompare.sh
+++ scripts/rtlcompare.sh
@@ -69,6 +69,81 @@ function diffBBox() {
     fi
 }
 
+function fitCanvas() {
+  IMG1=$1
+  IMG2=$2
+  BBOX1=$3
+  BBOX2=$4
+
+  set -- $BBOX1
+  ulx1=$1
+  uly1=$2
+  lrx1=$3
+  lry1=$4
+
+  set -- $BBOX2
+  ulx2=$1
+  uly2=$2
+  lrx2=$3
+  lry2=$4
+
+  ulxc=0
+  ulyc=0
+  lrxc=0
+  lryc=0
+
+  rpx1=0
+  rpy1=0
+  rpw1=0
+  rph1=0
+
+  rpx2=0
+  rpy2=0
+  rpw2=0
+  rph2=0
+
+  if [ $ulx1 -lt $ulx2 ]; then
+    rpx2=`calc "$ulx2-$ulx1" 0`
+    rpx1=0
+  else
+    rpx1=`calc "$ulx1-$ulx2" 0`
+    rpx2=0
+  fi
+
+  if [ $uly1 -lt $uly2 ]; then
+    rpy2=`calc "$uly2-$uly1" 0`
+    rpy1=0
+  else
+    rpy1=`calc "$uly1-$uly2" 0`
+    rpy2=0
+  fi
+
+  rpw1=`calc "$lrx1-$ulx1" 0`
+  rpw2=`calc "$lrx2-$ulx2" 0`
+  rph1=`calc "$lry1-$uly1" 0`
+  rph2=`calc "$lry2-$uly2" 0`
+
+  if [ $rpw1 -gt $rpw2 ]; then
+    rpw1=$rpw1
+    rpw2=$rpw1
+  else
+    rpw1=$rpw2
+    rpw2=$rpw2
+  fi
+
+  if [ $rph1 -gt $rph2 ]; then
+    rph1=$rph1
+    rph2=$rph1
+  else
+    rph1=$rph2
+    rph2=$rph2
+  fi
+
+
+  convert "$IMG1" -extent ${rpw1}x${rph1}+${rpx1}+${rpy1} "$IMG1"
+  convert "$IMG2" -extent ${rpw2}x${rph2}+${rpx2}+${rpy2} "$IMG2"
+}
+
 RES1=`$CC_PCLINT -dinfo -a -r "$F1-r.png" -v "$F1-v.png" "$F1" | grep "|"`
 RES2=`$CC_PCLINT -dinfo -a -r "$F2-r.png" -v "$F2-v.png" "$F2" | grep "|"`
 
@@ -128,77 +203,7 @@ R_BBOX2=`filter "$R_RES2" "bounding" | cut -d"=" -f2`
 
 R_BBOXDIFF=`diffBBox "$R_BBOX1" "$R_BBOX2"`
 
-
-set -- $R_BBOX1
-ulx1=$1
-uly1=$2
-lrx1=$3
-lry1=$4
-
-set -- $R_BBOX2
-ulx2=$1
-uly2=$2
-lrx2=$3
-lry2=$4
-
-ulxc=0
-ulyc=0
-lrxc=0
-lryc=0
-
-rpx1=0
-rpy1=0
-rpw1=0
-rph1=0
-
-rpx2=0
-rpy2=0
-rpw2=0
-rph2=0
-
-if [ $ulx1 -lt $ulx2 ]; then
-  rpx2=`calc "$ulx2-$ulx1" 0`
-  rpx1=0
-else
-  rpx1=`calc "$ulx1-$ulx2" 0`
-  rpx2=0
-fi
-
-if [ $uly1 -lt $uly2 ]; then
-  rpy2=`calc "$uly2-$uly1" 0`
-  rpy1=0
-else
-  rpy1=`calc "$uly1-$uly2" 0`
-  rpy2=0
-fi
-
-rpw1=`calc "$lrx1-$ulx1" 0`
-rpw2=`calc "$lrx2-$ulx2" 0`
-rph1=`calc "$lry1-$uly1" 0`
-rph2=`calc "$lry2-$uly2" 0`
-
-if [ $rpw1 -gt $rpw2 ]; then
-  rpw1=$rpw1
-  rpw2=$rpw1
-else
-  rpw1=$rpw2
-  rpw2=$rpw2
-fi
-
-if [ $rph1 -gt $rph2 ]; then
-  rph1=$rph1
-  rph2=$rph1
-else
-  rph1=$rph2
-  rph2=$rph2
-fi
-
-#echo $R_BBOX1
-#echo $R_BBOX2
-
-#echo $rpx1 $rpy1 $rpw1 $rph1 $rpx2 $rpy2 $rpw2 $rph2
-
-convert "$F1-r.png" -extent ${rpw1}x${rph1}+${rpx1}+${rpy1} "$F1-r.png"
-convert "$F2-r.png" -extent ${rpw2}x${rph2}+${rpx2}+${rpy2} "$F2-r.png"
+fitCanvas "$F1-r.png" "$F2-r.png" "$R_BBOX1" "$R_BBOX2"
+fitCanvas "$F1-v.png" "$F2-v.png" "$V_BBOX1" "$V_BBOX2"
 
 echo "$V_PDDIFF $V_WORKDIFF $V_MOVEDIFF $V_SEGDIFF $V_BBOXDIFF $R_PDDIFF $R_WORKDIFF $R_MOVEDIFF $R_SEGDIFF $R_BBOXDIFF"
