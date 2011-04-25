ed1593c66b27b99eb3146a17a12ee35c40eb633c
Marius Kintel <marius@kintel.net>
2011-04-25 12:56:09 +0200

    optimization: don't output a tmp image from compare

--- scripts/compare-bitmaps.sh
+++ scripts/compare-bitmaps.sh
@@ -12,7 +12,7 @@ shift $(($OPTIND - 1))
 
 # FIXME: Sometimes, compare fails when comparing very small images (e.g. 40 x 10 pixels).
 # It's unknown why this happens..
-pixelerror=`compare -fuzz 10% -metric AE $1 $2 tmp.png 2>&1`
+pixelerror=`compare -fuzz 10% -metric AE $1 $2 null: 2>&1`
 if [ $? -ne 0 ]; then
     echo "General error: Ouch"
     exit 1 # Compare failed to read image
