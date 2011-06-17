e20d956d7f1d61153d4d6c74dcb2d925c022b748
Marius Kintel <marius@kintel.net>
2011-06-18 00:47:13 +0200

    missing file

---
+++ src/gui/GroupItem.h
@@ -0,0 +1,23 @@
+#ifndef GROUPITEM_H_
+#define GROUPITEM_H_
+
+#include <QGraphicsItem>
+#include <QPainter>
+#include <QDebug>
+
+class GroupItem : public QGraphicsItemGroup
+{
+public:
+  GroupItem(QGraphicsItem *parent = 0) : QGraphicsItemGroup(parent) { }
+  void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) {
+    painter->setPen(Qt::red);
+    painter->drawRect(boundingRect());    
+  }
+  void addToGroup (QGraphicsItem *item) {
+    qDebug() << "before width=" << QGraphicsItemGroup::boundingRect().width() << "height=" << QGraphicsItemGroup::boundingRect().height();
+    QGraphicsItemGroup::addToGroup(item);
+    qDebug() << "after  width=" << QGraphicsItemGroup::boundingRect().width() << "height=" << QGraphicsItemGroup::boundingRect().height();
+  }
+};
+
+#endif
