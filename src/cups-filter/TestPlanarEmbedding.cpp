de5a87f04ed230195fdb4006cd29a3686267c3f1
kallaballa <amir@viel-zu.org>
2011-07-03 02:54:33 +0200

    src/cups-filter/TestPlanarEmbedding.cpp

---
+++ src/cups-filter/TestPlanarEmbedding.cpp
@@ -0,0 +1,16 @@
+#include "vector/model/CutModel.h"
+#include "vector/graph/Traverse.h"
+#include "vector/geom/Geometry.h"
+
+int main() {
+  CutModel planar;
+  CutSettings settings(0,0,0);
+  planar.createSegment(0, 0, 1, 0, settings);
+  planar.createSegment(1, 0, 1, 1, settings);
+  planar.createSegment(1, 1, 0, 1, settings);
+  planar.createSegment(0, 1, 0, 0, settings);
+
+  StringList strings;
+  make_linestrings(strings, planar.begin(), planar.end());
+  dump_linestrings(std::cerr, strings.begin(), strings.end());
+}
