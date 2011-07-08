82f9e4a5dce48fc547bbb98b27a44113a6133976
kallaballa <amir@viel-zu.org>
2011-07-08 20:57:38 +0200

    test if a graph is complete and well formed

--- src/cups-filter/vector/graph/Traverse.h
+++ src/cups-filter/vector/graph/Traverse.h
@@ -52,4 +52,65 @@ inline void traverse_planar_faces(SegmentGraph& graph, Visitor& visitor) {
   planar_face_traversal(graph, &embedding[0], visitor,make_assoc_property_map(e_index_map));
 }
 
+template<typename Graph>
+struct VertexPair {
+  typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
+  Vertex lower;
+  Vertex higher;
+  VertexPair() {
+
+  }
+  VertexPair(Vertex v1, Vertex v2) {
+    if(v1 == v2)
+      assert(!"source and target vertex are equal");
+
+    if(v1 < v2) {
+      this->lower = v1;
+      this->higher = v2;
+    } else {
+      this->lower = v2;
+      this->higher = v1;
+    }
+  }
+
+  bool operator<(const VertexPair& other) const {
+    return this->lower < other.lower || (this->lower == other.lower && this->higher < other.higher);
+  }
+};
+
+template<typename Graph>
+inline bool is_complete_graph(Graph& graph) {
+  typename graph_traits<Graph>::edge_iterator ei, ei_end;
+  typedef typename boost::graph_traits<Graph>::edge_descriptor Edge;
+  typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
+  typedef VertexPair<Graph> VPair;
+  std::set<VPair > uniqVertexPairs;
+
+  for(boost::tie(ei, ei_end) = edges(graph); ei != ei_end; ++ei) {
+    Edge edge = *ei;
+    VPair vpair(source(edge, graph), target(edge, graph));
+    if(!uniqVertexPairs.insert(vpair).second) {
+      assert(!"duplicate edge found");
+    }
+  }
+
+  typename graph_traits<Graph>::vertex_iterator vi, vi_end;
+  typename graph_traits<Graph>::vertex_iterator vj, vj_end;
+  Vertex i,j;
+
+  for(boost::tie(vi, vi_end) = vertices(graph); vi != vi_end; ++vi) {
+    i = *vi;
+    for(boost::tie(vj, vj_end) = vertices(graph); vj != vj_end; ++vj) {
+      j = *vj;
+      if(i == j)
+        continue;
+
+      if(!boost::edge(i, j,graph).second)
+        return false;
+    }
+  }
+
+  return true;
+}
+
 #endif /* TRAVERSE_H_ */
