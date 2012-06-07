917a0f4cfaa575722161e380fc0f72bc32fd3450
kallaballa <amir@viel-zu.org>
2012-06-07 18:07:16 +0200

    implemented merge operation

---
+++ src/ctrl-cut/cut/operations/Merge.hpp
@@ -0,0 +1,49 @@
+/*
+ * Ctrl-Cut - A laser cutter CUPS driver
+ * Copyright (C) 2009-2010 Amir Hassan <amir@viel-zu.org> and Marius Kintel <marius@kintel.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef MERGE_H_
+#define MERGE_H_
+
+#include <cut/model/Cut.hpp>
+#include "cut/geom/Geometry.hpp"
+#include <boost/foreach.hpp>
+
+inline void merge(Cut& src, Cut& sink) {
+  namespace tl = boost::geometry::strategy::transform;
+  Point pFrom = src.get(CutSettings::CPOS);
+  Point pInto = sink.get(CutSettings::CPOS);
+
+  if(pFrom != pInto) {
+    tl::translate_transformer<Point, Point> transformer(pFrom.x + (pInto.x * -1), pFrom.y + (pInto.y * -1));
+
+    BOOST_FOREACH(const Segment& seg, segments(src)) {
+      Segment translated;
+      boost::geometry::transform(seg, translated, transformer);
+      append(sink, translated);
+    }
+  } else {
+    BOOST_FOREACH(const Segment& seg, segments(src)) {
+      append(sink, seg);
+    }
+  }
+
+  sink.prepare();
+}
+
+#endif
