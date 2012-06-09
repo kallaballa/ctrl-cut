8be6b7c7d0cacb62880671418517842ed096ca42
kallaballa <amir@viel-zu.org>
2012-06-09 22:08:51 +0200

    formatting

--- src/ctrl-cut/encoder/HPGLEncoder.cpp
+++ src/ctrl-cut/encoder/HPGLEncoder.cpp
@@ -28,29 +28,12 @@
 using boost::format;
 
 void HPGLEncoder::encode(std::ostream &out, Cut& encodee) {
-  string filename = encodee.get(DocumentSettings::FILENAME);
-  Cut reduced = make_from(encodee);
-  Cut sorted = make_from(encodee);
-  Cut final = make_from(encodee);
-
-  if(encodee.get(CutSettings::OPTIMIZE) == CutSettings::INNER_OUTER)
-    traverse_onion(encodee, sorted);
-  else if(encodee.get(CutSettings::OPTIMIZE) == CutSettings::SHORTEST_PATH)
-    nearest_path_sorting(encodee, sorted);
-
-  plot_svg(sorted, filename + "_sorted");
-
-  const Point&  pos = sorted.get(CutSettings::CPOS);
-  translate(sorted,final, pos);
-
-  plot_svg(sorted, filename + "_translate");
-
   bool firstOperation = true;
   bool writingPolyline = false;
 
-  int power_set = final.get(CutSettings::CPOWER);
-  int speed_set = final.get(CutSettings::CSPEED);
-  int freq_set = final.get(CutSettings::FREQUENCY);
+  int power_set = encodee.get(CutSettings::CPOWER);
+  int speed_set = encodee.get(CutSettings::CSPEED);
+  int freq_set = encodee.get(CutSettings::FREQUENCY);
 
   out << V_INIT << SEP;
   out << format(V_POWER) % power_set << SEP;
@@ -61,83 +44,83 @@ void HPGLEncoder::encode(std::ostream &out, Cut& encodee) {
   int lastX = -1, lastY = -1;
   int lastPower = power_set;
 
-    BOOST_FOREACH(const Segment& seg, segments(final)) {
-      int power = power_set;// (seg.get(S_SET::S_POWER) != 0) ? seg.get(S_SET::S_POWER) : power_set;
-      if (power != lastPower) {
-        if (writingPolyline) {
-          out << SEP;
-          writingPolyline = false;
-        }
-        int epower = (power * power_set + 50) / 100;
-        if (speed_set && speed_set < 100) {
-          int espeed = speed_set;
-          int efreq = freq_set;
-          if (epower && power < 100) {
-            int r;
-            int q;
-            r = 10000 / power; // power, up to set power level (i.e. x=100)
-            q = 10000 / espeed;
-            if (q < r)
-              r = q;
-            q = 500000 / efreq;
-            if (q < r)
-              r = q;
-            epower = (50 + epower * r) / 100;
-            espeed = (50 + espeed * r) / 100;
-            efreq = (50 + espeed * r) / 100;
-          }
-
-          out << SEP << format(V_SPEED) % espeed;
-          out << SEP << format(V_FREQUENCY) % efreq << SEP;
-        }
-        out << format(V_POWER) % epower << SEP;
+  BOOST_FOREACH(const Segment& seg, segments(encodee)) {
+    int power = power_set;// (seg.get(S_SET::S_POWER) != 0) ? seg.get(S_SET::S_POWER) : power_set;
+    if (power != lastPower) {
+      if (writingPolyline) {
+        out << SEP;
+        writingPolyline = false;
       }
-
-      int startX = seg[0][0];
-      int startY = seg[0][1];
-      int endX = seg[1][0];
-      int endY = seg[1][1];
-
-      //     if (beginX < 0) {
-      //       beginX = startX;
-      //       beginY = startY;
-      //     }
-
-      // After a power change, always issue a PU, even if the current
-      // coordinate doesn't change.
-      if (lastX != startX || lastY != startY || lastPower != power) {
-        if (firstOperation) {
-          // FIXME: This is to emulate the LT bug in the Epilog drivers:
-          // Check if any clipping has been done in any of the passes, and
-          // inject the stray "LT" string. This has no function, just for bug compatibility
-          // of the output files. See corresponding FIXME in LaserJob.cpp.
-/*REFACTOR          if (!model.wasClipped()) */
-          out << HPGL_LINE_TYPE;
-          firstOperation = false;
-        } else {
-          out << SEP;
+      int epower = (power * power_set + 50) / 100;
+      if (speed_set && speed_set < 100) {
+        int espeed = speed_set;
+        int efreq = freq_set;
+        if (epower && power < 100) {
+          int r;
+          int q;
+          r = 10000 / power; // power, up to set power level (i.e. x=100)
+          q = 10000 / espeed;
+          if (q < r)
+            r = q;
+          q = 500000 / efreq;
+          if (q < r)
+            r = q;
+          epower = (50 + epower * r) / 100;
+          espeed = (50 + espeed * r) / 100;
+          efreq = (50 + espeed * r) / 100;
         }
-        out << HPGL_PEN_UP;
-        beginX = startX;
-        beginY = startY;
-        out << format("%d,%d") % startX % startY << SEP;
-        out << HPGL_PEN_DOWN << format("%d,%d") % endX % endY;
-        writingPolyline = true;
-      }
-      // ..else continue on polyline
-      else {
-        out << format(",%d,%d") % endX % endY;
-      }
 
-      lastX = endX;
-      lastY = endY;
-      lastPower = power;
-
-      // FIXME: This is a temporary hack to emulate the Epilog Windows driver,
-      // which appears to repeat the first vertex in a closed polyline twice at the end.
-      if (beginX == lastX && beginY == lastY)
-        out << format(",%d,%d") % beginX % beginY;
+        out << SEP << format(V_SPEED) % espeed;
+        out << SEP << format(V_FREQUENCY) % efreq << SEP;
+      }
+      out << format(V_POWER) % epower << SEP;
     }
+
+    int startX = seg[0][0];
+    int startY = seg[0][1];
+    int endX = seg[1][0];
+    int endY = seg[1][1];
+
+    //     if (beginX < 0) {
+    //       beginX = startX;
+    //       beginY = startY;
+    //     }
+
+    // After a power change, always issue a PU, even if the current
+    // coordinate doesn't change.
+    if (lastX != startX || lastY != startY || lastPower != power) {
+      if (firstOperation) {
+        // FIXME: This is to emulate the LT bug in the Epilog drivers:
+        // Check if any clipping has been done in any of the passes, and
+        // inject the stray "LT" string. This has no function, just for bug compatibility
+        // of the output files. See corresponding FIXME in LaserJob.cpp.
+/*REFACTOR          if (!model.wasClipped()) */
+        out << HPGL_LINE_TYPE;
+        firstOperation = false;
+      } else {
+        out << SEP;
+      }
+      out << HPGL_PEN_UP;
+      beginX = startX;
+      beginY = startY;
+      out << format("%d,%d") % startX % startY << SEP;
+      out << HPGL_PEN_DOWN << format("%d,%d") % endX % endY;
+      writingPolyline = true;
+    }
+    // ..else continue on polyline
+    else {
+      out << format(",%d,%d") % endX % endY;
+    }
+
+    lastX = endX;
+    lastY = endY;
+    lastPower = power;
+
+    // FIXME: This is a temporary hack to emulate the Epilog Windows driver,
+    // which appears to repeat the first vertex in a closed polyline twice at the end.
+    if (beginX == lastX && beginY == lastY)
+      out << format(",%d,%d") % beginX % beginY;
+  }
 
   if (writingPolyline) out << SEP;
   out << HPGL_END;
