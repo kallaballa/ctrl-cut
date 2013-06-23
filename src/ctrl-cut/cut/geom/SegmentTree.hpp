f04d0a3a2b5167acd392d0467275803784989893
kallaballa <amir@viel-zu.org>
2013-06-23 18:16:27 +0000

    keep track of singular iterators and fixed operator== of indexedsegmentnode

--- src/ctrl-cut/cut/geom/SegmentTree.hpp
+++ src/ctrl-cut/cut/geom/SegmentTree.hpp
@@ -37,36 +37,48 @@ template<
   template<typename > class Tallocator = std::allocator
 >
 struct IndexedSegmentNodeImpl: public Tcontainer<Segment, Tallocator<Segment> >::iterator {
+private:
+  typedef IndexedSegmentNodeImpl<Tcontainer, Tallocator> _Self;
   typedef typename Tcontainer<Segment, Tallocator<Segment> >::iterator _Base;
-  Point center;
-  uint32_t range;
+
+  bool _singular;
+  Point _center;
+  uint32_t _range;
+public:
 
   IndexedSegmentNodeImpl(const Point& center) :
-    _Base(), center(center) {
+    _Base(), _singular(true), _center(center) {
   }
 
   IndexedSegmentNodeImpl(const _Base it) :
-    _Base(it) {
-	Sphere sphere(*it);
-	center = sphere.center;
-	range = sphere.radius;
+    _Base(it), _singular(false) {
   }
 
   IndexedSegmentNodeImpl(const _Base it, bool isEnd) :
-    _Base(it) {
-	if(!isEnd){
-		Sphere sphere(*it);
-		center = sphere.center;
-		range = sphere.radius;
-	}
+    _Base(it), _singular(isEnd) {
+  }
+
+  const Point center() const {
+    if(this->_singular)
+      return this->_center;
+    const Sphere sphere(**this);
+    return sphere.center;
+  }
+
+  const Coord_t range() const {
+    if(this->_singular)
+      return this->_range;
+
+    const Sphere sphere(**this);
+    return sphere.radius;
   }
 
   bool operator==(const IndexedSegmentNodeImpl& other) const {
-    return **this == *other;
+    return ((this->_singular == true) && (other._singular == true)) || ((!this->_singular && !other._singular) && _Base::operator==(other));
   }
 
   static inline Coord_t centerComponent(const IndexedSegmentNodeImpl& item, const int& k) {
-    return item.center[k];
+    return item.center()[k];
   }
 };
 
@@ -86,7 +98,7 @@ public:
 
   template<typename ToutIter>
   void findWithinRange(const _SegmentNode& node, ToutIter outIter) const {
-    kdtree.find_within_range(node.center, node.range, outIter);
+    kdtree.find_within_range(node.center(), node.range(), outIter);
   }
 
   void insert(const _SegmentNode& node) {
@@ -97,7 +109,7 @@ public:
     typename KDTree_t::iterator it = kdtree.find_exact(node);
     // FIXME lost segments - but they don't affect the model since they would have been removed anyway
     if(it != kdtree.end())
-      kdtree.erase_exact(node);
+      kdtree.erase(it);
   }
 };
 template<
@@ -125,12 +137,16 @@ public:
   }
 
   void push_back(const Segment& seg) {
+    for(typename _SegmentIndex::iterator it = index.begin(); it != index.end(); ++it) {
+      if(*it == seg || *it == seg.swap())
+        return;
+    }
+
     index.push_back(seg);
     _Parent::insert(iterator(--(index.end()), false));
   }
 
   iterator erase(iterator& node) {
-    std::cerr << "Erase:" << *node << std::endl;
     _Parent::erase(node);
    return index.erase(node);
   }
@@ -148,6 +164,7 @@ typedef SegmentTreeImpl<
     SegmentNodeImpl,
     std::pointer_to_binary_function<const SegmentNodeImpl&, const int&, Coord_t>
 > SegmentTree;
+
 typedef IndexedSegmentTreeImpl<
     IndexedSegmentNodeImpl<std::list, std::allocator>,
     std::pointer_to_binary_function<
