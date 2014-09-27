9609c80d33800c289b9bfd9902a328219cf2b5c2
Marius Kintel <marius@kintel.net>
2014-09-26 21:13:43 -0400

    don't leak symbols to the global namespace

--- src/ctrl-cut/engrave/image/MMapMatrix.hpp
+++ src/ctrl-cut/engrave/image/MMapMatrix.hpp
@@ -24,30 +24,30 @@
 #include <boost/interprocess/mapped_region.hpp>
 #include <boost/interprocess/file_mapping.hpp>
 
-using namespace boost::interprocess;
+namespace interproc = boost::interprocess;
 
 template<class T>
 class MMapMatrix : public Image<T> {
 public:
   std::string filename;
-  file_mapping* m_file;
-  mapped_region m_region;
+  interproc::file_mapping* m_file;
+  interproc::mapped_region m_region;
   std::size_t size;
 
   MMapMatrix(std::string filename, uint32_t width, uint32_t height, uint8_t components = 3, uint64_t region_off = 0) :
     Image<T>(width, height, components), filename(filename) {
     LOG_DEBUG(region_off);
-    this->m_file = new file_mapping(filename.c_str(), read_write);
-    this->m_region = mapped_region(*this->m_file, read_write, 
+    this->m_file = new interproc::file_mapping(filename.c_str(), interproc::read_write);
+    this->m_region = interproc::mapped_region(*this->m_file, interproc::read_write, 
                                    region_off, width * height * this->bytes_per_pixel);
     this->addr = m_region.get_address();
     this->size = m_region.get_size();
     LOG_DEBUG(this->size);
   }
 
-  MMapMatrix(file_mapping* m_file, uint32_t width, uint32_t height, uint8_t components = 3) :
+  MMapMatrix(interproc::file_mapping* m_file, uint32_t width, uint32_t height, uint8_t components = 3) :
     Image<T>(width, height, components), filename(filename), m_file(m_file) {
-    this->m_region = mapped_region(*this->m_file, read_write, 
+    this->m_region = interproc::mapped_region(*this->m_file, interproc::read_write, 
                                    0, width * height * this->bytes_per_pixel);
     this->addr = m_region.get_address();
     this->size = m_region.get_size();
