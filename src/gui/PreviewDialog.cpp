06aa025188de8e605aea27be45b1b18b205e41fc
kallaballa <amir@viel-zu.org>
2014-10-08 19:31:23 +0200

    use a temp file for preview

--- src/gui/PreviewDialog.cpp
+++ src/gui/PreviewDialog.cpp
@@ -2,8 +2,8 @@
 #include <QString>
 #include <assert.h>
 #include "svg/SvgWriter.hpp"
-#include <sstream>
-
+#include <fstream>
+#include <boost/filesystem.hpp>
 PreviewDialog::PreviewDialog(Document& doc, QWidget *parent) : QDialog(parent)
 {
   setupUi(this);
@@ -11,11 +11,12 @@ PreviewDialog::PreviewDialog(Document& doc, QWidget *parent) : QDialog(parent)
   typedef DocumentSettings DS;
   Document copy = doc;
   copy.optimize();
-  std::stringstream ss;
-  SvgWriter svg(copy.get(DS::WIDTH).in(PX), copy.get(DS::HEIGHT).in(PX), copy.get(DS::RESOLUTION), copy.get(DS::TITLE), ss);
+  string filename = (boost::filesystem::temp_directory_path() /= boost::filesystem::unique_path()).native() + ".svg";
+  std::ofstream os(filename);
+  SvgWriter svg(copy.get(DS::WIDTH).in(PX), copy.get(DS::HEIGHT).in(PX), copy.get(DS::RESOLUTION), copy.get(DS::TITLE), os);
   svg.write(copy);
-
-  this->webView->setHtml(QString::fromStdString(ss.str()));
+  std::cerr << "fn:" << filename << std::endl;
+  this->webView->setUrl(QUrl::fromLocalFile(QString::fromStdString(filename)));
   this->webView->setZoomFactor(0.05);
   this->zoomBox->setCurrentIndex(10);
   connect(this->zoomBox, SIGNAL(currentIndexChanged(const QString&)), this, SLOT(on_zoom_triggered(const QString&)));
