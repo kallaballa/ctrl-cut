46c67f77a94ff5bfb769ac859272401c2a917dc4
kallaballa <amir@viel-zu.org>
2014-10-08 15:17:51 +0200

    make constructor with ostream available in svgwriter

--- src/render2cut/Render2Cut.cpp
+++ src/render2cut/Render2Cut.cpp
@@ -34,8 +34,8 @@ int main(int argc, char *argv[]) {
 
   Document doc;
   CupsOptions cupsOpts = CupsGetOpt::load_document(doc, argc, argv);
-
-  SvgWriter svgW(doc.get(DS::WIDTH).in(PX), doc.get(DS::HEIGHT).in(PX), doc.get(DS::RESOLUTION), doc.get(DS::TITLE), "/dev/stdout");
+  
+  SvgWriter svgW(doc.get(DS::WIDTH).in(PX), doc.get(DS::HEIGHT).in(PX), doc.get(DS::RESOLUTION), doc.get(DS::TITLE), std::cout);
   svgW.write(doc, "stroke:rgb(255,0,0);stroke-width:5;");
 
   clock_t end = clock() - start;
