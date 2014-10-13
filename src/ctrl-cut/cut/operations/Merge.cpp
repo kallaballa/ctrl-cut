bf3e89a2ec8987e88a2e9c73f7ed9a03d33411b3
kallaballa <amir@viel-zu.org>
2014-10-13 15:29:05 +0200

    implemented merge command

---
+++ src/ctrl-cut/cut/operations/Merge.cpp
@@ -0,0 +1,28 @@
+/*
+ * Ctrl-Cut - Laser cutter toolchain
+ * See LICENSE file
+ * Copyright (C) 2011 Amir Hassan <amir@viel-zu.org> and Marius kintel <kintel@kintel.net>
+ */
+
+#include "Merge.hpp"
+
+void merge(Cut& src, Cut& sink) {
+  namespace tl = boost::geometry::strategy::transform;
+  Point pFrom = src.get(CutSettings::CPOS);
+  Point pInto = sink.get(CutSettings::CPOS);
+
+  if(pFrom != pInto) {
+    tl::translate_transformer<Point, Point> transformer(pFrom.x + (pInto.x * -1), pFrom.y + (pInto.y * -1));
+
+    for(const SegmentPtr seg : segments(src)) {
+      Segment translated;
+      boost::geometry::transform(*seg.get(), translated, transformer);
+      append(sink, translated);
+    }
+  } else {
+    for(const SegmentPtr seg : segments(src)) {
+      append(sink, *seg.get());
+    }
+  }
+}
+
