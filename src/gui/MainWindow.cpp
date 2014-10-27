505c34878b939fdef5e5cb1a3c87c12dbf2ec72a
kallaballa <amir@viel-zu.org>
2014-10-27 11:58:18 +0100

    fixed about text

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -561,9 +561,9 @@ MainWindow::on_helpAboutAction_triggered()
                            QString("Copyright (C) 2009-2014 Amir Hassan <amir@viel-zu.org> and Marius Kintel <marius@kintel.net>\n"
                                    "\n"
                                    "This program is free software; you can redistribute it and/or modify"
-                                   "it under the terms of the GNU General Public License as published by"
-                                   "the Free Software Foundation; either version 2 of the License, or"
-                                   "(at your option) any later version."));
+                                   " it under the terms of the GNU General Public License as published by"
+                                   " the Free Software Foundation; either version 2 of the License, or"
+                                   " (at your option) any later version."));
 }
 
 void MainWindow::on_itemMoved(QGraphicsItem *movedItem,
