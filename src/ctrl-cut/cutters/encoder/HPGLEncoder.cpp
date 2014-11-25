7c84da99defe59b1ffc6db5ece40e29b9ff3d7e7
kallaballa <amir@viel-zu.org>
2014-11-25 13:55:17 +0100

    emit a hpgl pass every thousand vertices

--- src/ctrl-cut/cutters/encoder/HPGLEncoder.cpp
+++ src/ctrl-cut/cutters/encoder/HPGLEncoder.cpp
@@ -17,49 +17,28 @@ void HPGLEncoder::encode(std::ostream &out, Cut& encodee) {
   bool firstOperation = true;
   bool writingPolyline = false;
 
+  int cnt = 0;
+
   int power_set = encodee.get(CutSettings::CPOWER);
   int speed_set = encodee.get(CutSettings::CSPEED);
   int freq_set = encodee.get(CutSettings::FREQUENCY);
 
-  out << V_INIT << SEP;
+/* out << V_INIT << SEP;
   out << format(V_POWER) % power_set << SEP;
   out << format(V_SPEED) % speed_set << SEP;
-  out << format(V_FREQUENCY) % freq_set << SEP;
+  out << format(V_FREQUENCY) % freq_set << SEP;*/
 
   int beginX = -1, beginY = -1;
   int lastX = -1, lastY = -1;
-  int lastPower = power_set;
 
   for(const SegmentPtr segPtr : segments(encodee)) {
-    int power = power_set;// (seg.get(S_SET::S_POWER) != 0) ? seg.get(S_SET::S_POWER) : power_set;
-    if (power != lastPower) {
-      if (writingPolyline) {
-        out << SEP;
-        writingPolyline = false;
-      }
-      int epower = (power * power_set + 50) / 100;
-      if (speed_set && speed_set < 100) {
-        int espeed = speed_set;
-        int efreq = freq_set;
-        if (epower && power < 100) {
-          int r;
-          int q;
-          r = 10000 / power; // power, up to set power level (i.e. x=100)
-          q = 10000 / espeed;
-          if (q < r)
-            r = q;
-          q = 500000 / efreq;
-          if (q < r)
-            r = q;
-          epower = (50 + epower * r) / 100;
-          espeed = (50 + espeed * r) / 100;
-          efreq = (50 + espeed * r) / 100;
-        }
-
-        out << SEP << format(V_SPEED) % espeed;
-        out << SEP << format(V_FREQUENCY) % efreq << SEP;
-      }
-      out << format(V_POWER) % epower << SEP;
+    if (cnt % 1000 == 0) {
+      out << HPGL_END;
+      out << V_INIT << SEP;
+      out << format(V_POWER) % power_set << SEP;
+      out << format(V_SPEED) % speed_set << SEP;
+      out << format(V_FREQUENCY) % freq_set << SEP;
+      firstOperation = true;
     }
 
     Segment& seg = *segPtr.get();
@@ -75,7 +54,7 @@ void HPGLEncoder::encode(std::ostream &out, Cut& encodee) {
 
     // After a power change, always issue a PU, even if the current
     // coordinate doesn't change.
-    if (lastX != startX || lastY != startY || lastPower != power) {
+    if (lastX != startX || lastY != startY) {
       if (firstOperation) {
         // FIXME: This is to emulate the LT bug in the Epilog drivers:
         // Check if any clipping has been done in any of the passes, and
@@ -101,12 +80,13 @@ void HPGLEncoder::encode(std::ostream &out, Cut& encodee) {
 
     lastX = endX;
     lastY = endY;
-    lastPower = power;
 
     // FIXME: This is a temporary hack to emulate the Epilog Windows driver,
     // which appears to repeat the first vertex in a closed polyline twice at the end.
     if (beginX == lastX && beginY == lastY)
       out << format(",%d,%d") % beginX % beginY;
+
+    ++cnt;
   }
 
   if (writingPolyline) out << SEP;
